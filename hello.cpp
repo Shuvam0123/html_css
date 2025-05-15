e1d508fe1d0b0b5d07686a664777399e2978dbde
bipashasapkota <bipashasapkota71@gmail.com>
2025-05-15 08:40:34 +0545

    project init

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    double radius,area;
+    const double PI=3.1468;
+    // Write C++ code here
+    cout << "Enter the radius of the circle:"<<endl;
+    cin>>radius;
+    area=PI*radius*radius;
+    cout<<"the area of the circle is:"<<endl<<area<<endl;
+
+    return 0;
+    
+    
+    
+}
+
+
+
